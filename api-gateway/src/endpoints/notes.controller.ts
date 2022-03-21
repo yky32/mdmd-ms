bf9ad4b053a8286b5c1b8f900f3447cb9d7c4e16
wayne.yu <wayne.yu@eqonex.com>
2022-03-21 16:26:06 +0800

    Add Endpoint

---
+++ api-gateway/src/endpoints/notes.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { NotesService } from '../modules/notes/notes.service';
+import { CreateNoteDto } from '../modules/notes/dto/create-note.dto';
+import { UpdateNoteDto } from '../modules/notes/dto/update-note.dto';
+import {ApiTags} from "@nestjs/swagger";
+
+@ApiTags("notes")
+@Controller('notes')
+export class NotesController {
+  constructor(private readonly notesService: NotesService) {}
+
+  @Post()
+  create(@Body() createNoteDto: CreateNoteDto) {
+    return this.notesService.create(createNoteDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.notesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.notesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateNoteDto: UpdateNoteDto) {
+    return this.notesService.update(+id, updateNoteDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.notesService.remove(+id);
+  }
+}
